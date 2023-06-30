445f77b08fd29ad5885206ad7657825dff40ea3f
Mpilo Simamane <Simamanempilom@gmail.com>
2023-06-30 13:53:11 +0200

    Detention done :)

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/5. Loops/Workbook 5.2/Detention.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/5. Loops/Workbook 5.2/Detention.java
@@ -1,8 +1,16 @@
+import java.util.Scanner;
+
 public class Detention {
     public static void main(String[] args) {
 
-        System.out.println("Hi Bart. I can write lines for you.\nWhat do you want me to write?");
-        // See Learn the Part for the instructions.
-        
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Hi Bart. I can write lines for you.\nWhat do you want me to write?\t");
+        String response = scan.nextLine();
+
+        for (int i = 1; i < 100; i++){
+            System.out.println(i + ". "+ response);
+        }
+        scan.close();
     }
 }
