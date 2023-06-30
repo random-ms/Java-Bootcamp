4591dc78510da6cbac69a46173d3613701305454
Mpilo Simamane <Simamanempilom@gmail.com>
2023-06-30 14:21:43 +0200

    Counting tool complete

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/5. Loops/Workbook 5.3/CountingTool.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/5. Loops/Workbook 5.3/CountingTool.java
@@ -4,8 +4,12 @@ public class CountingTool {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        // See Learn the Part for the instructions.
+        System.out.print("Hi Timmy! Choose a number to count to: \t");
+        int response = scan.nextInt();
 
+        for (int i = 0; i <= response; i++){
+            System.out.print(i + " ");
+        }
         scan.close();
     }
 }
