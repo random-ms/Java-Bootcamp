89e2f429221e73c1524634cd158f8d92b3871494
random-ms <137601910+random-ms@users.noreply.github.com>
2023-07-01 08:33:01 +0200

    CounterTool completed

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/5. Loops/Workbook 5.5/CounterTool.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/5. Loops/Workbook 5.5/CounterTool.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class CounterTool {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("I hear you like to count by threes\n");
+        System.out.println("I hear you like to count by threes.\n");
         System.out.println("\nJimmy: It depends.");
         System.out.println("Oh, ok...");
 
