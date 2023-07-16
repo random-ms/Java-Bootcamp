f1996f5186f6af2716e50da72dda720f3b0a5b6a
random-ms <137601910+random-ms@users.noreply.github.com>
2023-07-16 08:47:43 +0200

    FizzBuzz correction

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/5. Loops/Workbook 5.7/FizzBuzz.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/5. Loops/Workbook 5.7/FizzBuzz.java
@@ -1,7 +1,7 @@
 public class FizzBuzz {
     public static void main(String[] args) {
         for (int i = 0; i < 19; i++){
-            if (i % 3 == 0 && i % 3 == 0){
+            if (i % 3 == 0 && i % 5 == 0){
                 System.out.println(i + " FizzBuzz");
             } else if (i % 3 == 0){
                 System.out.println(i + " Fizz");
