fcec9860f8fde78603f655fb49d88094c6b153cb
random-ms <137601910+random-ms@users.noreply.github.com>
2023-07-17 18:43:29 +0200

    Nested Intro done

--- Sections/Section 5/NestedLoops.java
+++ Sections/Section 5/NestedLoops.java
@@ -1,5 +1,12 @@
 public class NestedLoops {
     public static void main(String[] args) {
         
+        for(int i = 0; i < 3; i++){
+            System.out.println("\nRun: " + i);
+            for(int j = 0; j < 3; j++){
+                System.out.println(j);
+            }
+        }
+        
     }
 }
