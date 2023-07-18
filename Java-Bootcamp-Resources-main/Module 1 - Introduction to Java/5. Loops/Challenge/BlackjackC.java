25991fc5d16b34875b09a5df65a4919605eec3c6
Mpilo Simamane <Simamanempilom@gmail.com>
2023-07-18 12:05:34 +0200

    Task 1 & 2 complete

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/5. Loops/Challenge/BlackjackC.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/5. Loops/Challenge/BlackjackC.java
@@ -5,10 +5,17 @@ public class BlackjackC {
     public static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
+
+        for (int i = 1; i < 6; i++) {
+            System.out.println(cardString(drawRandomCard()));  
+        }
+
         System.out.println("\nWelcome to Java Casino!");
         System.out.println("Do you have a knack for Black Jack?");
         System.out.println("We shall see..");
         System.out.println("..Ready? Press anything to begin!");
+
+        
         //Task 3 – Wait for the user to press enter.
         //Task 4 – Get two random cards.
         //       – Print them: \n You get a \n" + <randomCard> + "\n and a \n" + <randomCard>
@@ -37,23 +44,110 @@ public class BlackjackC {
 
     }
 
-    /** Task 1 – make a function that returns a random number between 1 and 13
-     * Function name – drawRandomCard
-     * @return (int)
-     *
-     * Inside the function:
-     *   1. Gets a random number between 1 and 13.
-     *   2. Returns a card.
-     */
+     public static int drawRandomCard(){
+        Double rc = Math.random() * 14;
+        int rCard = rc.intValue();
+        return rCard;
+     }
 
-    /** Task 2 – make a function that returns a String drawing of the card.
-     * Function name – cardString
-     * @param cardNumber (int)
-     * @return (String)
-     *
-     * Inside the function:
-     *   1. Returns a String drawing of the card.
-     */
+    public static String cardString(int cardNumber){
+
+        switch (cardNumber){
+            case 1: return  "   _____\n"+
+                            "  |A _  |\n"+ 
+                            "  | ( ) |\n"+
+                            "  |(_'_)|\n"+
+                            "  |  |  |\n"+
+                            "  |____V|\n";
+                
+            case 2: return  "   _____\n"+              
+                            "  |2    |\n"+ 
+                            "  |  o  |\n"+
+                            "  |     |\n"+
+                            "  |  o  |\n"+
+                            "  |____Z|\n";
+
+            case 3: return  "   _____\n" +
+                            "  |3    |\n"+
+                            "  | o o |\n"+
+                            "  |     |\n"+
+                            "  |  o  |\n"+
+                            "  |____E|\n";
+
+            case 4: return  "   _____\n" +
+                            "  |4    |\n"+
+                            "  | o o |\n"+
+                            "  |     |\n"+
+                            "  | o o |\n"+
+                            "  |____h|\n";
+                         
+            case 5: return  "   _____ \n" +
+                            "  |5    |\n" +
+                            "  | o o |\n" +
+                            "  |  o  |\n" +
+                            "  | o o |\n" +
+                            "  |____S|\n";
+                                      
+            case 6: return  "   _____ \n" +
+                            "  |6    |\n" +
+                            "  | o o |\n" +
+                            "  | o o |\n" +
+                            "  | o o |\n" +
+                            "  |____6|\n";
+                           
+            case 7: return  "   _____ \n" +
+                            "  |7    |\n" +
+                            "  | o o |\n" +
+                            "  |o o o|\n" +
+                            "  | o o |\n" +
+                            "  |____7|\n";
+
+            case 8: return  "   _____ \n" +
+                            "  |8    |\n" +
+                            "  |o o o|\n" +
+                            "  | o o |\n" +
+                            "  |o o o|\n" +
+                            "  |____8|\n";
+            
+            case 9: return  "   _____ \n" +
+                            "  |9    |\n" +
+                            "  |o o o|\n" +
+                            "  |o o o|\n" +
+                            "  |o o o|\n" +
+                            "  |____9|\n";
+
+        
+            case 10: return "   _____ \n" +
+                            "  |10  o|\n" +
+                            "  |o o o|\n" +
+                            "  |o o o|\n" +
+                            "  |o o o|\n" +
+                            "  |___10|\n";
+                           
+            case 11: return "   _____\n" +
+                            "  |J  ww|\n"+ 
+                            "  | o {)|\n"+ 
+                            "  |o o% |\n"+ 
+                            "  | | % |\n"+ 
+                            "  |__%%[|\n";
+        
+            case 12: return "   _____\n" +
+                            "  |Q  ww|\n"+ 
+                            "  | o {(|\n"+ 
+                            "  |o o%%|\n"+ 
+                            "  | |%%%|\n"+ 
+                            "  |_%%%O|\n";
+            
+            case 13: return "   _____\n" +
+                            "  |K  WW|\n"+ 
+                            "  | o {)|\n"+ 
+                            "  |o o%%|\n"+ 
+                            "  | |%%%|\n"+ 
+                            "  |_%%%>|\n";
+
+            default: return "Unlikely to happen.";           
+        }
+    }
 
     public static String faceDown() {
         return
