149928146607efd8146af50f2ce993e4831a389b
Mpilo Simamane <Simamanempilom@gmail.com>
2023-07-18 11:25:10 +0200

    More changes

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/5. Loops/Pokerito/PokeritoP.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/5. Loops/Pokerito/PokeritoP.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class PokeritoA {
+public class PokeritoP {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/5. Loops/Workbook 5.1/Megaphone.java
+++
@@ -1,8 +0,0 @@
-public class Megaphone {
-    public static void main(String[] args) {
-       for (int i = 1; i < 11; i++){
-        System.out.println(i + ". If Java was easy, they would call it Python!");
-       }
-    }
-    
-}
