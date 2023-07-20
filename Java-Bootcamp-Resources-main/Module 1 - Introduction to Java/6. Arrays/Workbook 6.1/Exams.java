952ba48f79050449f7bcd89a2c54a32c92d16c3f
Mpilo Simamane <Simamanempilom@gmail.com>
2023-07-20 15:21:07 +0200

    6.1 done

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/6. Arrays/Workbook 6.1/Exams.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/6. Arrays/Workbook 6.1/Exams.java
@@ -1,17 +1,14 @@
 public class Exams {
     public static void main(String[] args) {
-
-        // Instructions for this workbook are on Learn the Part (Workbook 6.1).
-
-        System.out.println("It's time to take your 5th year exams. Please, take your seats.\n");
         
-        System.out.println("<student at index 0>, you will take seat " + 0);
-        System.out.println("<student at index 1>, you will take seat " + 1);
-        System.out.println("<student at index 2>, you will take seat " + 2);
-        System.out.println("<student at index 3>, you will take seat " + 3);
-        System.out.println("<student at index 4>, you will take seat " + 4);
+        System.out.println("It's time to take your 5th year exams. Please, take your seats.\n");
 
-        // Compare your result to what's on Learn the Part.
-
+        String[] seats = {"Harry", "Neville", "Ron", "Hermione", "Seamus"};
+        
+        System.out.println(seats[0] + ", you will take seat " + 0);
+        System.out.println(seats[1] + ", you will take seat " + 1);
+        System.out.println(seats[2] + ", you will take seat " + 2);
+        System.out.println(seats[3] + ", you will take seat " + 3);
+        System.out.println(seats[4] + ", you will take seat " + 4);
     }
 }
