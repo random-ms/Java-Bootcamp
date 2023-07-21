4a68cb232778a82e4b470892da1f312e2d390d57
Mpilo Simamane <Simamanempilom@gmail.com>
2023-07-21 09:41:42 +0200

    Progress...

--- Sections/Section 6/UpdateArrays.java
+++ Sections/Section 6/UpdateArrays.java
@@ -1,6 +1,9 @@
+import java.util.Arrays;
+
 public class UpdateArrays {
     public static void main(String[] args){
         String[] menu = {"Espresso", "Iced Coffee", "Macchiato"};
+        System.out.println(Arrays.toString(menu));
     }
     
 }
