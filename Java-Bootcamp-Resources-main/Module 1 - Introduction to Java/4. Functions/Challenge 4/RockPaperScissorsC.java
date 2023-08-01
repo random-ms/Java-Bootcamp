283be7c98e0a14c7e201bcb7048992f382214d26
Mpilo Simamane <Simamanempilom@gmail.com>
2023-08-01 09:10:41 +0200

    Class correction

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/4. Functions/Challenge 4/RockPaperScissorsC.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/4. Functions/Challenge 4/RockPaperScissorsC.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class RockPaperScissors {
+public class RockPaperScissorsC {
     public static void main(String[] args) {
         
         Scanner scan = new Scanner(System.in);
