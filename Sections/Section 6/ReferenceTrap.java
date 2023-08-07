656156041b0d3633b2d23e76f0898f2274e03ca2
Mpilo Simamane <Simamanempilom@gmail.com>
2023-08-07 09:04:00 +0200

    Solution

--- Sections/Section 6/ReferenceTrap.java
+++ Sections/Section 6/ReferenceTrap.java
@@ -1,5 +1,17 @@
+import java.util.Arrays;
+
 public class ReferenceTrap {
     public static void main(String[] args) {
+        String[] staffLastYear = {"Tommy", "Joel","Ellie"};
+        String[] staffThisYear = new String[3];
+
+        for (int i = 0; i < staffThisYear.length; i++) {
+            staffThisYear[i] = staffLastYear[i];
+        }
+
+        staffThisYear[1] = "Abby";
+        System.out.println(Arrays.toString(staffLastYear));
+        System.out.println(Arrays.toString(staffThisYear));
     }
     
 }
