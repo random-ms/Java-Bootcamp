673c2fb79e7be6df09c3958f685715c8e3ef6be8
Mpilo Simamane <Simamanempilom@gmail.com>
2023-08-08 09:54:38 +0200

    old way Prices

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/6. Arrays/Workbook 6.13/Prices.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/6. Arrays/Workbook 6.13/Prices.java
@@ -1,5 +1,26 @@
 public class Prices {
     public static void main(String[] args) {
-        // The instructions for this workbook are on Learn the Part (workbook 6.13)
+        double[][] prices = new double[3][5];
+        prices[0][0] = 12.99;
+        prices[0][1] = 8.99;
+        prices[0][2] = 9.99;
+        prices[0][3] = 10.49;
+        prices[0][4] = 11.99;
+
+        prices[1][0] = 0.99;
+        prices[1][1] = 1.99;
+        prices[1][2] = 2.49;
+        prices[1][3] = 1.49;
+        prices[1][4] = 2.49;
+
+        prices[2][0] = 8.99;
+        prices[2][1] = 7.99;
+        prices[2][2] = 9.49;
+        prices[2][3] = 9.99;
+        prices[2][4] = 10.99;
+        
+        System.out.println("Baking: " + prices[0][0] + " " + prices[0][1] + " " + prices[0][2] + " " + prices[0][3] + " " + prices[0][4]);
+        System.out.println("Beverage: " + prices[1][0] + " " + prices[1][1] + " " + prices[1][2] + " " + prices[1][3] + " " + prices[1][4]);
+        System.out.println("Cereal: " + prices[2][0] + " " + prices[2][1] + " " + prices[2][2] + " " + prices[2][3] + " " + prices[2][4]);
     }
 }
