ebbf350effa983385286aac45ed3ec94b8f74305
Mpilo Simamane <Simamanempilom@gmail.com>
2023-08-08 11:16:19 +0200

    Complete :)

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/6. Arrays/Workbook 6.14/RandomNumbers.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/6. Arrays/Workbook 6.14/RandomNumbers.java
@@ -1,13 +1,14 @@
 public class RandomNumbers {
     public static void main(String[] args) {
-        int[][] array = {
-            {48, 56, 56, 76, 0, 81, 51, 81, 99, 70},
-            {38, 52, 73, 6, 10, 56, 1, 71, 47, 9},
-            {85, 35, 47, 98, 91, 25, 69, 52, 2, 93}
-        };
+        int[][] array = new int[100][10];
 
-        print2DArray(array);
-     
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++){
+                array[i][j] = randomNumber();
+            }
+        }
+
+            print2DArray(array);
     }
 
         public static int randomNumber() {
