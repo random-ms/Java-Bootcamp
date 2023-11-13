955f489a80936bbb23a9eff0ee214dfed209d780
Mpilo Simamane <Simamanempilom@gmail.com>
2023-11-13 15:54:16 +0200

    RV3 Happy

--- Sections/Section 4/ReturnValues3.java
+++ Sections/Section 4/ReturnValues3.java
@@ -2,46 +2,72 @@ import java.util.Scanner;
 
 public class ReturnValues3 {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
 
+        Scanner scan = new Scanner(System.in);
+        double length = 0;
+        double width = 0;
+        
         System.out.println("Hi, please enter prefered language output 1-3: \t" +
-        "\n1. English \n2. French \n3. Spanish\t");
+        "\n1. English \n2. French \n3. Spanish \t");
 
         int choice = scan.nextInt();
-        String langChoice = explainArea(choice);
-        System.out.println("\n" + langChoice);
 
-        System.out.println("\nPlease enter length: \t");
-        Double length = scan.nextDouble();
-        
-        System.out.println("\nPlease enter width: \t");
-        Double width = scan.nextDouble();
-
-        Double area = calculateArea(length, width);
-        printArea(length, width, area);
-
-        scan.close();
-        }
-
-        public static String explainArea(int language){
-            switch (language) {
-                case 1: return "Area equals length * width"; //English
-                case 2: return "La surface est egale a la longueur * la largeur"; //French
-                case 3: return "area es igual a largo * ancho"; //Spanish
-                default: return "Language not available.";
+            if (choice == 1){
+                System.out.println("\nArea equals length * width");
+                System.out.println("\nPlease enter length: \t");
+                length = scan.nextDouble();
+                System.out.println("\nPlease enter width: \t");
+                width = scan.nextDouble();
             }
-        }
-    
-        public static double calculateArea(double length, double width){
-            if (length < 0 || width < 0){
-                System.out.println("Invalid Dimension.");
+
+            if (choice == 2) {
+                System.out.println("\nLa surface est egale a la longueur * la largeur");
+                System.out.println("\nVeuillez entrer la longueur: \t");
+                length = scan.nextDouble();
+                System.out.println("\nVeuillez entrer la largeur: \t");
+                width = scan.nextDouble();
             } 
+
+            if (choice == 3){
+                System.out.println("a\nrea es igual a largo * ancho");
+                System.out.println("\nPor favor ingrese la longitud \t");
+                length = scan.nextDouble();
+                System.out.println("\nPor favor ingrese el ancho: \t");
+                width = scan.nextDouble();
+            }
+            
+            Double area = calculateArea(choice, length, width);
+            printArea(choice, length, width, area);
+
+            scan.close();
+        
+    }
+    
+        public static double calculateArea(int choice, double length, double width){
+            if (length < 0 || width < 0){
+                if(choice==1){
+                        System.out.println("Invalid Dimension.");
+                }
+                if (choice == 2){
+                System.out.println("Dimension invalide.");
+                }
+                if (choice ==3){
+                System.out.println("Dimensión no válida");
+                }
+            }
                 double area = length * width;
                 return area;
-        } 
-
-
-        public static void printArea(double length, double width, double area){
-            System.out.println("\nA rectangle with a length of " + length + ", and a width of " + width + ", has an area of " + area);
         }
-    }
+        
+        public static void printArea(int choice, double length, double width, double area){
+            if (choice == 1){
+                System.out.println("\nA rectangle with a length of " + length + ", and a width of " + width + ", has an area of " + area);
+            }
+            if (choice == 2) {
+                System.out.println("\nUn rectangle d'une longueur de " + length + ", et une largeur de " + width + ", a une superficie de " + area);
+            }
+            if (choice ==3){
+                System.out.println("\nUn rectángulo con una longitud de " + length + ", y un ancho de " + width + ", tiene un área de " + area);
+            }
+        }
+}
