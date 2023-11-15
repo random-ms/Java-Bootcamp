39fc81f91e700b69437541aa390aa427ef2132f4
random-ms <137601910+random-ms@users.noreply.github.com>
2023-11-15 09:21:15 +0200

    Progress - added break

--- Sections/Section 4/ReturnValues3.java
+++ Sections/Section 4/ReturnValues3.java
@@ -7,52 +7,58 @@ public class ReturnValues3 {
         double length = 0;
         double width = 0;
         
-        System.out.println("Hi, please enter prefered language output 1-3: \t" +
+        System.out.println("Hi, please enter prefered i output 1-3: \t" +
         "\n1. English \n2. French \n3. Spanish \t");
 
         int choice = scan.nextInt();
 
-            if (choice == 1){
-                System.out.println("\nArea equals length * width");
-                System.out.println("\nPlease enter length: \t");
-                length = scan.nextDouble();
-                System.out.println("\nPlease enter width: \t");
-                width = scan.nextDouble();
+            for (int i = choice; i < 4; i++){
+                if (i == 1){
+                    System.out.println("\nArea equals length * width");
+                    System.out.println("\nPlease enter length: \t");
+                    length = scan.nextDouble();
+                    System.out.println("\nPlease enter width: \t");
+                    width = scan.nextDouble();
+                    break;
+                }
+    
+                if (i == 2) {
+                    System.out.println("\nLa surface est egale a la longueur * la largeur");
+                    System.out.println("\nVeuillez entrer la longueur: \t");
+                    length = scan.nextDouble();
+                    System.out.println("\nVeuillez entrer la largeur: \t");
+                    width = scan.nextDouble();
+                    break;
+                } 
+    
+                if (i == 3){
+                    System.out.println("a\nrea es igual a largo * ancho");
+                    System.out.println("\nPor favor ingrese la longitud \t");
+                    length = scan.nextDouble();
+                    System.out.println("\nPor favor ingrese el ancho: \t");
+                    width = scan.nextDouble();
+                    break;
+                }
+                scan.close();
             }
 
-            if (choice == 2) {
-                System.out.println("\nLa surface est egale a la longueur * la largeur");
-                System.out.println("\nVeuillez entrer la longueur: \t");
-                length = scan.nextDouble();
-                System.out.println("\nVeuillez entrer la largeur: \t");
-                width = scan.nextDouble();
-            } 
-
-            if (choice == 3){
-                System.out.println("a\nrea es igual a largo * ancho");
-                System.out.println("\nPor favor ingrese la longitud \t");
-                length = scan.nextDouble();
-                System.out.println("\nPor favor ingrese el ancho: \t");
-                width = scan.nextDouble();
-            }
-            
             Double area = calculateArea(choice, length, width);
             printArea(choice, length, width, area);
-
-            scan.close();
-        
     }
     
         public static double calculateArea(int choice, double length, double width){
-            if (length < 0 || width < 0){
-                if(choice==1){
-                        System.out.println("Invalid Dimension.");
+            for (int i = choice; length < 0 || width < 0; i++){
+                if(i == 1){
+                    System.out.println("Invalid Dimension.");
+                    break;
                 }
-                if (choice == 2){
-                System.out.println("Dimension invalide.");
+                if (i == 2){
+                    System.out.println("Dimension invalide.");
+                    break;
                 }
-                if (choice ==3){
-                System.out.println("Dimensión no válida");
+                if (i == 3){
+                    System.out.println("Dimensión no válida");
+                    break;
                 }
             }
                 double area = length * width;
@@ -60,14 +66,19 @@ public class ReturnValues3 {
         }
         
         public static void printArea(int choice, double length, double width, double area){
-            if (choice == 1){
-                System.out.println("\nA rectangle with a length of " + length + ", and a width of " + width + ", has an area of " + area);
-            }
-            if (choice == 2) {
-                System.out.println("\nUn rectangle d'une longueur de " + length + ", et une largeur de " + width + ", a une superficie de " + area);
-            }
-            if (choice ==3){
-                System.out.println("\nUn rectángulo con una longitud de " + length + ", y un ancho de " + width + ", tiene un área de " + area);
+            for (int i = choice; i < 4; i++){
+                if (i == 1){
+                    System.out.println("\nA rectangle with a length of " + length + ", and a width of " + width + ", has an area of " + area);
+                    break;
+                }
+                if (i == 2) {
+                    System.out.println("\nUn rectangle d'une longueur de " + length + ", et une largeur de " + width + ", a une superficie de " + area);
+                    break;
+                }
+                if (i == 3){
+                    System.out.println("\nUn rectángulo con una longitud de " + length + ", y un ancho de " + width + ", tiene un área de " + area);
+                    break;
+                }
             }
         }
 }
