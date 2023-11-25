05dddf3d8db8610b7c6badd11b5bfe6da0486a32
random-ms <137601910+random-ms@users.noreply.github.com>
2023-11-25 23:45:57 +0200

    folder changes

---
+++ Sections - M2/Section 7/Main.java
@@ -0,0 +1,13 @@
+public class Main extends Car{
+    public static void main(String[] args) {
+        Car nissan = new Car();
+        nissan.make = "Nissan";
+        nissan.price = 10000;
+        nissan.year = "Nissan";
+        nissan.color = "Nissan";
+
+
+        
+    }
+    
+}
