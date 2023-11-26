62f8017acdb62973b041456757de23aaa8e1d12d
random-ms <137601910+random-ms@users.noreply.github.com>
2023-11-26 08:17:11 +0200

    saved

--- Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/7. The World of Objects/workbooks/Workbook 7.1/Main.java
+++ Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/7. The World of Objects/workbooks/Workbook 7.1/Main.java
@@ -1,5 +1,23 @@
 public class Main {
 
-    Person person = new Person();
+    public static void main(String[] args) {
+
+        Person person = new Person();
+
+        person.name = "Mr Simamane";
+        person.nationality = "South African";
+        person.dateOfBirth = "19 July 0000";
+        person.passport = new String[]{person.name, person.nationality, person.dateOfBirth};
+        person.seatNumber = 5;
+
+        System.out.println(person.name);
+        System.out.println(person.nationality);
+        System.out.println(person.dateOfBirth);
+        System.out.println(Arrays.toString(person.passport));
+        System.out.println(person.seatNumber);
+            
+    }
+
+    
 
 }
