caf689822f6bf25909cbab1089bffe1c7301475e
random-ms <137601910+random-ms@users.noreply.github.com>
2023-12-03 19:15:39 +0200

    progress

--- Sections - M2/Section 7/Car2.java
+++ Sections - M2/Section 7/Car2.java
@@ -1,15 +1,19 @@
+import java.util.Arrays;
+
 public class Car2 {
 
     private String make;
     private double price;
     private int year;
     private String color;
+    private String[] parts;
 
-    public Car2(String make, double price, int year, String color){
+    public Car2(String make, double price, int year, String color, String[] parts){
         this.make = make;
         this.price = price;
         this.year = year;
         this.color = color;
+        this.parts = Arrays.copyOf(parts, parts.length);
     }
 
     public Car2(Car2 source){
@@ -17,6 +21,7 @@ public class Car2 {
         this.price = source.price;
         this.year = source.year;
         this.color = source.color;
+        this.parts = Arrays.copyOf(source.parts, source.parts.length);
     }
     
     public String getMake(){
@@ -49,6 +54,14 @@ public class Car2 {
 
     public void setColor(String color){
         this.color = color;
+    }
+
+    public String[] getParts(){
+        return Arrays.copyOf(this.parts, this.parts.length);
+    }
+
+    public void setParts(String[] parts){
+        this.parts = Arrays.copyOf(parts, parts.length);
     }
 
     
--- Sections - M2/Section 7/Main2.java
+++ Sections - M2/Section 7/Main2.java
@@ -1,10 +1,10 @@
 public class Main2 {
 
     public static void main(String[] args) {
+        
+    Car2 nissan = new Car2("Nissan", 10000, 2020, "Green", new String[]{"Tires", "Keys"});
 
-    Car2 nissan = new Car2("Nissan", 10000, 2020, "Green");
-
-    Car2 dodge = new Car2("Dodge", 11000, 2019, "Blue");
+    Car2 dodge = new Car2("Dodge", 11000, 2019, "Blue", new String[]{"Tires", "Keys"});
 
     Car2 nissan2 = new Car2(nissan);
 
