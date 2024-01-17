e32ec63144f7a40552455ff4db21df3728151acb
Mpilo Simamane <Simamanempilom@gmail.com>
2024-01-17 08:44:01 +0200

    changes added new contact file

--- Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/7. The World of Objects/extra-practice/Part 1/Main.java
+++ Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/7. The World of Objects/extra-practice/Part 1/Main.java
@@ -1,24 +1,4 @@
-public class Contact {
-
-    private String name;
-    private String phoneNumber;
-    private String birthDate;
-    private int age;
-
-    public Contact(String name, String phoneNumber, String birthDate, int age){
-        this.name = name;
-        this.phoneNumber = phoneNumber;
-        this.birthDate = birthDate;
-        this.age = age;
-    }
-
-    public Contact(Contact source){
-        this.name = source.name;
-        this.phoneNumber = source.phoneNumber;
-        this.birthDate = source.birthDate;
-        this.age = source.age;
-    }
-
+public class Main {
     public static void main(String[] args) {
         Contact contact1 = new Contact("Alice", "123-456-7890", "1990-01-01", 33);
         Contact contact2 = new Contact("Bob", "234-567-8901", "1992-02-02", 31);
@@ -27,7 +7,5 @@ public class Contact {
 
         Contact copyContact1 = new Contact(contact1);
         Contact copyContact2 = new Contact(contact3);
-    
     }
-    
 }
