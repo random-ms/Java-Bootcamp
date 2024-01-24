b9dbbc200febcfeeb9310f0a6e4666ff56dd6d81
Mpilo Simamane <Simamanempilom@gmail.com>
2024-01-24 21:14:31 +0200

    example 2 completed

--- Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/10. Exception Handling/Lessons/checked-exceptions/ExampleTwo.java
+++ Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/10. Exception Handling/Lessons/checked-exceptions/ExampleTwo.java
@@ -1,12 +1,25 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class ExampleTwo {
     public static void main(String[] args) {
 
+        try{
+            readFile("greetings.txt");
+        } catch (FileNotFoundException e){
+            System.out.println(e.getMessage());
+        } finally {
+            System.out.println("Process Complete!");
+        }
+
     }
     
-    public static void readFile(String fileName) {
-        // FileInputStream fis = new FileInputStream("greetings.txt");
-        // Scanner scanner = new Scanner(fis);
-        // System.out.println(scanner.nextLine());
-        // scanner.close();
+    public static void readFile(String fileName) throws FileNotFoundException {
+        FileInputStream fis = new FileInputStream("Java-Bootcamp-Resources-main\\Module 2 - Object Oriented Programming" + 
+                                                        "\\10. Exception Handling\\Lessons\\checked-exceptions\\" + fileName);
+        Scanner scanner = new Scanner(fis);
+        System.out.println(scanner.nextLine());
+        scanner.close();
     }
 }
