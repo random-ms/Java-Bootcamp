ad3986ee5af90439110a47a0b481525b33d166ee
Mpilo Simamane <Simamanempilom@gmail.com>
2024-02-12 17:53:12 +0200

    rename

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/6. Arrays/Challenge 6/TTT/TTT/Logic.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/6. Arrays/Challenge 6/TTT/TTT/Logic.java
@@ -1,39 +1,9 @@
 package TTT;
 import java.util.Scanner;
 
-public class TicTacToe {
+public class Logic {
     static Scanner scan = new Scanner(System.in);
-    public static void main(String[] args) {
-        
-      System.out.println("\nLet's play tic tac toe");
-      char[][] board = {
-                {'_','_','_'},
-                {'_','_','_'},
-                {'_','_','_'}
-      };
 
-      printBoard(board);
-
-      for (int i = 0; i < 9; i++) {
-
-        if(i%2 == 0){
-          System.out.println("Turn: X");
-          int[]spot = askUser(board);
-          board[spot[0]][spot[1]] = 'X';
-        } else {
-          System.out.println("Turn: O");
-          int[]spot = askUser(board);
-          board[spot[0]][spot[1]] = 'O';
-        }
-        printBoard(board);
-
-        int count = checkWin(board);
-        if (count == 3) { System.out.println("X Wins!"); break; }
-        else if (count == -3) { System.out.println("O Wins!"); break; }
-        else System.out.println("IT'S A TIE!");
-      }
-      scan.close();
-    }
 
     public static void printBoard(char[][] board){
       System.out.println("\n"); 
--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/6. Arrays/Challenge 6/TTT/TTT/Main.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/6. Arrays/Challenge 6/TTT/TTT/Main.java
@@ -1,5 +1,42 @@
 package TTT;
 
+import java.util.Scanner;
+
 public class Main {
+
+    static Scanner scan = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        
+        System.out.println("\nLet's play tic tac toe");
+        char[][] board = {
+                    {'_','_','_'},
+                    {'_','_','_'},
+                    {'_','_','_'}
+        };
+
+        Logic.printBoard(board);
+
+        for (int i = 0; i < 9; i++) {
+
+            if(i%2 == 0){
+            System.out.println("Turn: X");
+            int[]spot = Logic.askUser(board);
+            board[spot[0]][spot[1]] = 'X';
+            } else {
+            System.out.println("Turn: O");
+            int[]spot = Logic.askUser(board);
+            board[spot[0]][spot[1]] = 'O';
+            }
+            Logic.printBoard(board);
+
+            int count = Logic.checkWin(board);
+    
+            if (count == 3) { System.out.println("X Wins!"); break; }
+            else if (count == -3) { System.out.println("O Wins!"); break; }
+            else System.out.println("IT'S A TIE!");
+        }
+        scan.close();
+    }
     
 }
