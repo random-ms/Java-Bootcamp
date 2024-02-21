d56b2a2eaf40656064a1949b957f5acb3c515fd2
Mpilo Simamane <Simamanempilom@gmail.com>
2024-02-21 16:46:07 +0200

    Completed

--- Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/11. Map Collections/Extra Practice/Part 1/Main.java
+++ Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/11. Map Collections/Extra Practice/Part 1/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        
+
+        City city1 = new City("New York", "USA", 8500000);
+        System.out.println(city1.getName());
+
+        // Uncomment to test invalid input for setName, setCountry, setCountry & input for the constructor
+        city1.setName("Zoo York");
+        city1.setCountry("Zoo");
+        city1.setPopulation(10000);
+        City city2 = new City("", "USA", 8500000);
+
+    
     }
 }
