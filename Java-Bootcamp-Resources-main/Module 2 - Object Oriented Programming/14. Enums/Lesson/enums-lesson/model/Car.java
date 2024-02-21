6da290497b95e337191e0e06548088c05e6ca1df
Mpilo Simamane <Simamanempilom@gmail.com>
2024-02-21 21:12:38 +0200

    ENUMS

--- Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/14. Enums/Lesson/enums-lesson/model/Car.java
+++ Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/14. Enums/Lesson/enums-lesson/model/Car.java
@@ -1,7 +1,9 @@
 package model;
+
 public class Car {
     private String make;
     private int year;
+    public enum TrafficLight {RED, GREEN, YELLOW};
 
     public Car(String make, int year) {
         this.make = make;
@@ -24,26 +26,12 @@ public class Car {
         this.year = year;
     }
 
-    public void drive(String trafficLight) {
-        if (!trafficLight.equals("RED") && !trafficLight.equals("GREEN") && !trafficLight.equals("YELLOW")) {
-            throw new IllegalArgumentException("TRAFFIC LIGHT CAN ONLY BE RED, GREEN, YELLOW");
-        }
-
+    public void drive(TrafficLight trafficLight) {
+        if(trafficLight == null) throw new IllegalArgumentException("Traffic Light cannot be null");
         switch (trafficLight) {
-            case "RED": System.out.println("STOP"); break;
-            case "GREEN": System.out.println("DRIVE"); break;
-            case "YELLOW": System.out.println("SLOW DOWN"); break;
+            case RED: System.out.println("STOP"); break;
+            case GREEN: System.out.println("DRIVE"); break;
+            case YELLOW: System.out.println("SLOW DOWN"); break;
         }
     }
-
-    // public void drive(TrafficLight trafficLight) {
-    //     if (trafficLight == null) throw new IllegalArgumentException("Traffic Light cannot be null");
-    //     switch (trafficLight) {
-    //         case RED: System.out.println("STOP"); break;
-    //         case GREEN: System.out.println("DRIVE"); break;
-    //         case YELLOW: System.out.println("SLOW DOWN"); break;
-    //     }
-    // }
-
-
 }
