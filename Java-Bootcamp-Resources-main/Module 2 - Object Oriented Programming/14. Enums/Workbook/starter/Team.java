68739269814e37deb423553a16bd9c60cc966bcd
Mpilo Simamane <Simamanempilom@gmail.com>
2024-02-21 21:45:07 +0200

    Progress...

--- Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/14. Enums/Workbook/starter/Team.java
+++ Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/14. Enums/Workbook/starter/Team.java
@@ -1,10 +1,12 @@
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.swing.text.Position;
+
 public class Team {
 
     private String name;
-    private Map<String, String> players;
+    private Map<Position, String> players;
 
     public Team(String name) {
         this.name = name;
@@ -16,20 +18,16 @@ public class Team {
     }
 
     public void setName(String name) {
-        if (name == null || name.isBlank())
-            throw new IllegalArgumentException("Name cannot be null/blank");
+        if (name == null) throw new IllegalArgumentException("Name cannot be null/blank");
         this.name = name;
     }
 
-    public String getPlayer(String position) {
+    public String getPlayer(Position position) {
         return this.players.get(position);
     }
 
-    public void setPlayer(String position, String player) {
-        if (!position.equals("SHOOTING_GUARD") || !position.equals("SMALL_FORWARD") || !position.equals("POWER_FORWARD") || !position.equals("CENTER") || !position.equals("POINT_GUARD")) {
-            throw new IllegalArgumentException("INVALID POSITION");
-        }
+    public void setPlayer(Position position, String player) {
+        if (position == null) throw new IllegalArgumentException("Position cannot be null");
         this.players.put(position, player);
-    }
-    
+    }   
 }
