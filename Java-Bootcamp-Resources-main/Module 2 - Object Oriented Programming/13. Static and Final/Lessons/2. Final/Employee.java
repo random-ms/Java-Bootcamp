ced688921ea9a9eb344df50a6986e04e73a2280c
Mpilo Simamane <Simamanempilom@gmail.com>
2024-02-21 19:27:08 +0200

    done with Final

--- Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/13. Static and Final/Lessons/2. Final/Employee.java
+++ Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/13. Static and Final/Lessons/2. Final/Employee.java
@@ -2,6 +2,8 @@ public class Employee {
 
     private String name;
     private int age;
+    public static final int MIN_AGE = 18;
+    public static final int MAX_AGE = 65;
     
     public Employee(String name, int age) {
         setAge(age);
@@ -22,7 +24,7 @@ public class Employee {
     }
 
     public void setAge(int age) {
-        if (age < 18 || age > 65) throw new IllegalArgumentException("INVALID AGE");
+        if (age < MIN_AGE || age > MAX_AGE) throw new IllegalArgumentException("INVALID AGE");
         this.age = age;
     }
 
--- Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/13. Static and Final/Lessons/2. Final/Main.java
+++ Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/13. Static and Final/Lessons/2. Final/Main.java
@@ -4,11 +4,11 @@ public class Main {
         Employee john = new Employee("John", 45);
         Employee lisa = new Employee("Lisa", 20);
 
-        System.out.println("Retirement Age: " + 65);
-        System.out.println("Minimum Working Age: " + 18);
+        System.out.println("Retirement Age: " + Employee.MAX_AGE);
+        System.out.println("Minimum Working Age: " + Employee.MIN_AGE);
 
-        System.out.println(john.getName() + ", you can retire in " + (65 - john.getAge()) + " years.");
-        System.out.println(lisa.getName() + ", you have been eligible to work for " + (lisa.getAge() - 18) + " years.");
+        System.out.println(john.getName() + ", you can retire in " + (Employee.MAX_AGE - john.getAge()) + " years.");
+        System.out.println(lisa.getName() + ", you have been eligible to work for " + (lisa.getAge() - Employee.MIN_AGE) + " years.");
 
     }
 }
