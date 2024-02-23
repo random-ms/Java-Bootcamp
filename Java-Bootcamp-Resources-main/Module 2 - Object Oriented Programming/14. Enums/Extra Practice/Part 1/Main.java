2cc0fe58914e56506ce86838495b0b8181f243b4
Mpilo Simamane <Simamanempilom@gmail.com>
2024-02-23 17:49:35 +0200

    Part 1 completed

--- Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/14. Enums/Extra Practice/Part 1/Main.java
+++ Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/14. Enums/Extra Practice/Part 1/Main.java
@@ -1,5 +1,28 @@
+import model.Car;
+
+import static model.Car.*;
+import static model.Car.BodyType.*;
+
 public class Main {
+    
     public static void main(String[] args) {
-        
+
+    Car car1 = new Car("Toyota", "Camry", SEDAN, 2020, 30_000);
+    System.out.println(car1.getMake());
+
+    // Uncomment to test invalid input for setMake, setModel, setBodyType, setProductionYear, setPrice
+    //car1.setMake("");
+    //car1.setModel("");
+    //car1.setBodyType(null);
+    //car1.setProductionYear(1899);
+    //car1.setPrice(-1);
+    //car1.setPrice(200001);
+
+    System.out.println(MIN_YEAR); // public static final... interesting. :)
+    System.out.println(MAX_PRICE);
+
+    // Uncomment to test invalid input for the constructor
+    Car car2 = new Car("BMW", "X5", COUPE, 2024, 45_000);
+    System.out.println(car2.getPrice());
     }
 }
