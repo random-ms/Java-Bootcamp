2b6bb3bf4d095bcfc5b451fff748e3893322442a
Mpilo Simamane <Simamanempilom@gmail.com>
2024-02-23 20:08:30 +0200

    Part 3 Complete

--- Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/14. Enums/Extra Practice/Part 3/Main.java
+++ Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/14. Enums/Extra Practice/Part 3/Main.java
@@ -1,5 +1,106 @@
+import static model.Car.*;
+
+import java.util.Scanner;
+
+import model.Car;
+import model.CarDealership;
+import model.Car.BodyType;
+
 public class Main {
+
+    Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        
+
+        Scanner scanner = new Scanner(System.in);
+    
+        String make = promptForMake(scanner);
+        String model = promptForModel(scanner);
+        BodyType bodyType = promptForBodyType(scanner);
+        int year = promptForYear(scanner);
+        double price = promptForPrice(scanner);
+    
+    
+        Car newCar = new Car(make, model, bodyType, year, price);
+    
+    
+        CarDealership dealership = new CarDealership();
+        dealership.addCar(newCar);
+    
+    
+        System.out.println("Car added to the dealership: " + newCar.getMake() + " " + newCar.getModel());
     }
+
+
+    public static boolean isNullOrBlank(String input) {
+        return input == null || input.isBlank();
+    }
+
+    public static boolean invalidYear(int year) {
+        return year < MIN_YEAR;
+    }
+
+    public static boolean invalidPrice(double price) {
+        return price < MIN_PRICE || price > MAX_PRICE;
+    }
+    
+    public static boolean invalidBodyType(String bodyType) {
+        try {
+            BodyType.valueOf(bodyType.toUpperCase());
+            return false;
+        } catch (Exception e){
+            return true;
+        }
+    }
+
+    public static BodyType promptForBodyType(Scanner scanner) {
+        while (true) {
+            System.out.print("\nPlease enter a valid car body type: ");
+            String bodyType = scanner.nextLine();
+            if (!invalidBodyType(bodyType)) return BodyType.valueOf(bodyType.toUpperCase());
+        }
+    }
+
+    public static String promptForMake(Scanner scanner) {
+        while (true) {
+            System.out.print("\nPlease enter a valid car make: ");
+            String make = scanner.nextLine();
+            if (!isNullOrBlank(make)) return make;
+        }
+    }
+
+    public static String promptForModel(Scanner scanner) {
+        while (true) {
+            System.out.print("\nPlease enter a valid car model: ");
+            String model = scanner.nextLine();
+            if(!isNullOrBlank(model)) return model;
+        }
+    }
+
+    public static int promptForYear(Scanner scanner) {
+        while (true) {
+            System.out.print("\nPlease enter a valid production year: ");
+            if (!scanner.hasNextInt()){
+                scanner.next();
+                continue;
+            }
+
+            int year = scanner.nextInt();
+            if (!invalidYear(year)) return year;
+        }
+    }
+
+    public static double promptForPrice(Scanner scanner) {
+        while (true) {
+            System.out.print("\nPlease enter a valid car price: ");
+
+            if(!scanner.hasNextDouble()){
+                scanner.next();
+                continue;
+            }
+
+            double price = scanner.nextDouble();
+            if (!invalidPrice(price)) return price;
+        }
+    }  
 }
