469ce83d926af5edef271809d9f717db75b91b5c
Mpilo Simamane <Simamanempilom@gmail.com>
2024-03-02 13:15:43 +0200

    Stream operation lesson

--- Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/16. Higher Order Functions/Lessons/Lesson Four/Main.java
+++ Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/16. Higher Order Functions/Lessons/Lesson Four/Main.java
@@ -5,6 +5,11 @@ public class Main {
     public static void main(String[] args) {
 
         List<Double> prices = Arrays.asList(341.67, 209.32, 88.41, 269.99, 68.49, 499.99, 28.12, 354.38);
-
+        prices.stream()
+            .filter(price -> price > 100)
+            .map(price -> price - 20)
+            .sorted((right, left) -> right.compareTo(left))
+            .map(price -> "$" + price)
+            .forEach(price -> System.out.println(price));
     }
 }
