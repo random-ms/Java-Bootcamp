5eee12dfd76cac63bbda8f13aa88a47d90a5906e
Mpilo Simamane <Simamanempilom@gmail.com>
2024-03-03 08:36:31 +0200

    259. completed

--- Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/16. Higher Order Functions/Lessons/Lesson Six/Main.java
+++ Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/16. Higher Order Functions/Lessons/Lesson Six/Main.java
@@ -1,15 +1,26 @@
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         
         // Creating Stream from Datasource: Array
         String[] greetings = new String[]{"Hello!", "Hola!", "Bonjour!", "Hallo!"};
+        Arrays.stream(greetings).forEach(x -> System.out.println(x));
 
+        System.out.println("\n");
 
         // Creating Stream from Datasource: File
-        Path path = Paths.get("path to file");
-
+        String path1 = "Java-Bootcamp-Resources-main\\Module 2 - Object Oriented Programming\\16. Higher Order Functions\\Lessons\\Lesson Six\\";
+        try {
+            Path path = Paths.get(path1 + "chorus.txt");
+            Files.lines(path).forEach(line -> System.out.println(line));
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+        
     }
 }
