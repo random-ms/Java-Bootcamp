405dfda92e72c8c0031e9f5ce9c775c9c23c146e
Mpilo Simamane <Simamanempilom@gmail.com>
2024-03-03 09:40:56 +0200

    Extra Completed HOF

--- Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/16. Higher Order Functions/Extra Practice/Part 1/model/MovieStore.java
+++ Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/16. Higher Order Functions/Extra Practice/Part 1/model/MovieStore.java
@@ -1,5 +1,36 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MovieStore {
 
+    private List<Movie> movies;
+
+    public MovieStore() {
+        this.movies = new ArrayList<>();
+    }
+
+    public void addMovie (Movie movie){
+        this.movies.add(movie);
+    }
+
+    public List<Movie> filterByGenre(String genre){
+        return movies.stream()
+                .filter(movie -> movie.getGenre().equalsIgnoreCase(genre))
+                .toList();
+    }
+
+    public List<Movie> sortByReleaseYear(){
+        return movies.stream()
+                .sorted((x, y) -> Integer.compare(x.getReleaseYear(), y.getReleaseYear()))
+                .toList();
+    }
+
+    public List<Movie> getTopRatedMovies(int n){
+        return movies.stream()
+                .sorted((x, y) -> Double.compare(y.getRating(), x.getRating()))
+                .limit(n) // limits to a number of entries / movies
+                .toList();
+    }
 }
