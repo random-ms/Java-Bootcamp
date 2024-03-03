f8fd6302f76ea90123c23ee1d8e973dbf601cb7a
Mpilo Simamane <Simamanempilom@gmail.com>
2024-03-03 09:50:55 +0200

    big D

--- Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/17. BigDecimal/Lesson/Decimals.java
+++ Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/17. BigDecimal/Lesson/Decimals.java
@@ -1,15 +1,14 @@
+import java.math.BigDecimal;
+
 public class Decimals {
     public static void main(String[] args) {
 
-        double x = 0.1;
+        BigDecimal x = new BigDecimal("0.1");
 
         System.out.println(x);
 
-        double y = 0.2;
-        double z = x + y;
+        BigDecimal y = new BigDecimal("0.2");
         
-        System.out.println(z);
-
+        System.out.println(x.add(y));
     }
-
 }
