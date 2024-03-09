14a57dd2ec6e2852aceea3f8c7d904b88415a297
Mpilo Simamane <Simamanempilom@gmail.com>
2024-03-09 17:32:23 +0200

    Daemon Threads

--- multithreading-resources-main/06-daemon-threads/Main.java
+++ multithreading-resources-main/06-daemon-threads/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Thread memoryMonitorThread = new Thread(new MemoryMonitorDaemon());
+        memoryMonitorThread.setDaemon(true); //basically ends after all tasks are completed.
         memoryMonitorThread.start();
 
         Thread memoryIntensiveTaskThread = new Thread(new MemoryIntensiveTask());
